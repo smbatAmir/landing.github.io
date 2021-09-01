dab15749fe32384cccaa8cd1afd5a6d3f5d5dd41
Victoria <victoria@webmaker.am>
2021-09-01 10:42:53 +0400

    _document error

--- pages/_document.js
+++ pages/_document.js
@@ -53,10 +53,11 @@ MyDocument.getInitialProps = async (ctx) => {
     // Render app and page and get the context of the page with collected side effects.
     const sheets = new ServerStyleSheets();
     const originalRenderPage = ctx.renderPage;
+    const appComponent = App => (props) => sheets.collect(<App {...props} />);
 
     ctx.renderPage = () =>
         originalRenderPage({
-            enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+            enhanceApp: appComponent,
         });
 
     const initialProps = await Document.getInitialProps(ctx);
